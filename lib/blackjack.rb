b41c8f86144e668fb73570a7bc01598ca4ef03c9
Devin Arnold <dbarnold321@gmail.com>
2019-05-07 01:27:11 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -23,8 +23,8 @@ def get_user_input
   # code #get_user_input here
 end
 
-def end_game(total)
-  puts "Sorry, you hit #{total}. Thanks for playing!"
+def end_game(card_total)
+  puts "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
 def initial_round
